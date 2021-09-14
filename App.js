723632526cc15b6574f85a10592ff69b8cc63c5a
Carpoforo <jdelgadilloz1905@gmail.com>
2021-09-13 22:02:29 -0400

    Creando proyecto de lectura de imagen

--- App.js
+++ App.js
@@ -1,21 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { useState, useEffect } from 'react'
+import { Button, Image, View, Platform } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+
+export default function ImagePickerExample() {
+	const [image, setImage] = useState(null)
+	const [googleResponse, setGoogleResponse] = useState(null)
+
+	useEffect(() => {
+		;(async () => {
+			if (Platform.OS !== 'web') {
+				const { status } =
+					await ImagePicker.requestMediaLibraryPermissionsAsync()
+				if (status !== 'granted') {
+					alert(
+						'Lo sentimos, necesitamos permisos de cámara para que esto funcione'
+					)
+				}
+			}
+		})()
+	}, [])
+
+	const pickImage = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		})
+
+		console.log(result)
+
+		if (!result.cancelled) {
+			setImage(result.uri)
+		}
+	}
+
+	const takePhoto = async () => {
+		let result = await ImagePicker.launchCameraAsync({
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		})
+
+		console.log(result)
+
+		if (!result.cancelled) {
+			setImage(result.uri)
+		}
+	}
+
+	const maybeRenderImage = () =>{
+
+		if(!image){
+			//return
+		}
+
+		return(
+			<View
+			style={{
+				marginTop : 20,
+				width:250,
+				borderRadius: 2
+			}}
+			>
+				<Button
+					style={{ marginBottom: 10}}
+					onPress={submitToGoogle}
+					title="Analizar"
+					/>
+
+					<View
+					style={{
+						borderTopRightRadius:3,
+						borderTopLeftRadius: 3,
+						shadowColor:'rgba(0,0,0,1)',
+						shadowOpacity:0.2,
+						shadowOffset:{ width:4, height: 4},
+						overflow:'hidden'
+
+					}}
+					>
+					<Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+					</View>
+
+			</View>
+		)
+	}
+
+	const submitToGoogle = async () =>{
+
+		try {
+			
+		} catch (error) {
+			
+			console.error(error)
+		}
+	}
+
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Button title='Elija una imagen' onPress={pickImage} />
+
+			<Button onPress={takePhoto} title='Toma una foto' />
+
+			{image && (
+				<Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+			)}
+			{!googleResponse && (
+					maybeRenderImage()
+				)
+			}
+		</View>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
